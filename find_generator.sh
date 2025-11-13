54545dc2d66c29ef239013e1b56fe2216ae1f7f0
Tiago Mazzutti <tiago.mazzutti@ifc.edu.br>
2025-11-13 11:30:49 -0300

    chore: regenerate 3D plots and caches

--- find_generator.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Find where PlotlyPlotter is actually used to create these files
-
-find src -name "*.py" -type f -exec grep -l "PlotlyPlotter\|save_figure\|write_html" {} \; | while read f; do
-    echo "=== $f ==="
-    grep -n "PlotlyPlotter\|save_figure\|write_html" "$f" | head -5
-done
